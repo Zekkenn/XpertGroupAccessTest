91999548441a9c07d52cd2e414c2a8c987cd6fce
Zekkenn <sebastian.mr47@gmail.com>
2019-07-07 11:49:30 -0500

    first commit

---
+++ src/main/java/xpertgroup/technical/accessTest/AccessTestApplication.java
@@ -0,0 +1,13 @@
+package xpertgroup.technical.accessTest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccessTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccessTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/xpertgroup/technical/accessTest/AccessTestApplicationTests.java
@@ -0,0 +1,16 @@
+package xpertgroup.technical.accessTest;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AccessTestApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
